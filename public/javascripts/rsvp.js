680da33333a5c8785317980f9bb394ad1778806b
Everardo Rosales <erosales@mit.edu>
2015-12-06 21:41:24 -0500

    Fixing rvsp always yes bug.

--- public/javascripts/rsvp.js
+++ public/javascripts/rsvp.js
@@ -31,7 +31,7 @@ var loadRsvpPage = function() {
 $(document).on("click",".rsvp-attend", function(e){
   e.preventDefault();
   var attending = true;
-  if ($(this).val() === 0){
+  if ($(this).val() === "0"){
     attending = false;
   }
   var event_id = $(this).attr("eventId");
